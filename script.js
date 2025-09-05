cf987aa5ba81f23f56aa60768e3a5344048067ba
joaoG15a <joao.eidam.amaral@escola.pr.gov.br>
2025-09-05 15:14:15 +0000

    aula 6

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const atual = document.querySelector('.ativo');
 const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
 atual.classList.remove('ativo');
 document.getElementById(proximoPasso).classList.add('ativo');
-
-
-
    } )
 })
 
